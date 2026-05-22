a434ca922c7922b2f889d9e3d3cd6b6799e438a7
Nitin Kumar <nitin22337@iiitd.ac.in>
2026-05-22 16:10:43 +0530

    first commit - Microservices Architecture

---
+++ services/auth-service/app/core/config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings
+from typing import Literal
+
+class Settings(BaseSettings):
+
+    DATABASE_URL:str
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+
+    INTERNAL_SECRET : str
+
+    ENVIRONMENT = Literal["development","staging","production"] = "development"
+    
+    class Config:
+        env_file = ".env"
+        case_sensitive = True
+
+settings = Settings()
